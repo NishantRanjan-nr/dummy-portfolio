938bf399f14036bbb5e38f2207f6a50758049bb6
NishantRanjan-nr <nishantranjan910@gmail.com>
2026-06-14 04:20:36 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+// JavaScript for interactive elements can be added here.
+// For example, a lightbox for the certificate gallery or animated statistics.
+
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Portfolio website loaded');
+});
